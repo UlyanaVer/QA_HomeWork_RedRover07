96ed9aecb7d58180456f555dab7f2acd1e74b2c2
UlyanaVer <uveryaskina@gmail.com>
2023-08-11 12:29:28 +0300

    Add lesson 2 HomeWork 3

---
+++ src/Lesson_2_variables/HW_3.java
@@ -0,0 +1,9 @@
+package Lesson_2_variables;
+
+public class HW_3 {
+    public static void main(String[] args) {
+        //Задача Экстра, с двумя звездочками
+        //Вывести/напечатать смайлик (не :), а настоящий смайлик одним символом).
+        System.out.println("\uD83D\uDE0A");
+    }
+}
