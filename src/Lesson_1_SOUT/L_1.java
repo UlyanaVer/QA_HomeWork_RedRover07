25c5af7296c187964c2d105ecbc31abeac7ff3d3
UlyanaVer <uveryaskina@gmail.com>
2023-08-11 11:31:57 +0300

    add Hello world

--- src/Lesson_1_SOUT/L_1.java
+++ src/Lesson_1_SOUT/L_1.java
@@ -1,2 +1,7 @@
-package Lesson_1_SOUT;public class L_1 {
+package Lesson_1_SOUT;
+
+public class L_1 {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
 }
