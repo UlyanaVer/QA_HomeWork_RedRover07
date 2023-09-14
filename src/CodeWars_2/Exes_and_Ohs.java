b7d1a736f73a66d5b8b321dcd53778a9f6fc3b35
UlyanaVer <uveryaskina@gmail.com>
2023-09-14 21:16:27 +0300

    CodeWars Task 2 done

--- src/CodeWars_2/Exes_and_Ohs.java
+++ src/CodeWars_2/Exes_and_Ohs.java
@@ -11,4 +11,29 @@ public class Exes_and_Ohs {
     //XO("ooxXm") => true
     //XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
     //XO("zzoo") => false
+    public static boolean getXO(String str) {
+        int countX = 0;
+        int countO = 0;
+        boolean answer = true;
+        String strLowCase = str.toLowerCase();
+
+        for (int i = 0; i < strLowCase.length(); i++) {
+            if (strLowCase.charAt(i) == 'x') {
+                countX++;
+            } else if (strLowCase.charAt(i) == 'o') {
+                countO++;
+            }
+        }
+        if (countX == countO) {
+            answer = true;
+        }else{
+            answer = false;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getXO("xxxoootrenOOOXXX"));
+        System.out.println(getXO("xxxoootrenOOOXX"));
+    }
 }
--- src/CodeWars_2/Main.java
+++ src/CodeWars_2/Main.java
@@ -1,4 +1,23 @@
 package CodeWars_2;
 
 public class Main {
+    public static void main(String[] args) {
+        String str = "ooo/oMxxxx";
+        int countX = 0;
+        int countO = 0;
+        String strLowCase = str.toLowerCase();
+
+        for (int i = 0; i < strLowCase.length(); i++) {
+            if (strLowCase.charAt(i) == 'x') {
+                countX++;
+            }else if (strLowCase.charAt(i) == 'o') {
+                countO++;
+            }
+            }
+        if (countX == countO) {
+            System.out.println("2true");
+        } else {
+            System.out.println("Not True");
+        }
+    }
 }
